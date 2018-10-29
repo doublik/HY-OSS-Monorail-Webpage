fbcd149a6a603ce127bac4907890c3ff86d0d9d0
Ikjun OH <0x696A4F@gmail.com>
2018-10-30 02:37:32 +0900

    add contents (history/intro)

--- oss/script.js
+++ oss/script.js
@@ -2,7 +2,7 @@ $("#before").click(function() {
     var active_id = $(".active_title").attr("id");
     $(".active_title").removeClass("active_title").addClass("topic_title");
     $(".active_label").removeClass("active_label").addClass("label");
-    $("#contents").load("introduction/" + (Number(active_id)-1) + ".html");
+    $("#contents").load("oss/" + (Number(active_id)-1) + ".html");
     $("#" + (Number(active_id)-1)).removeClass("topic_title").addClass("active_title");
     $(".label:has(.active_title)").removeClass("label").addClass("active_label");
 });
@@ -11,7 +11,7 @@ $("#next").click(function() {
     var active_id = $(".active_title").attr("id");
     $(".active_title").removeClass("active_title").addClass("topic_title");
     $(".active_label").removeClass("active_label").addClass("label");
-    $("#contents").load("introduction/" + (Number(active_id)+1) + ".html");
+    $("#contents").load("oss/" + (Number(active_id)+1) + ".html");
     $("#" + (Number(active_id)+1)).removeClass("topic_title").addClass("active_title");
     $(".label:has(.active_title)").removeClass("label").addClass("active_label");
 });
